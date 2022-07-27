0523a19e9f2f193168cd1b865102bf92dd91c9e2
Frankie Stroud <fstroud@aviatrix.com>
2022-07-27 23:09:51 +1000

    version

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ data "aws_region" "current" {}
  
  module "mc_aws_transit" {
   #source = "git::https://github.com/terraform-aviatrix-modules/terraform-aviatrix-mc-transit"
-  source  = "terraform-aviatrix-modules/mc-transit/aviatrix//examples/aws_ha"
+  source  = "terraform-aviatrix-modules/mc-transit/aviatrix/"
   version = "2.1.6"
   cloud                  = "AWS"
   cidr                   = var.aws_transit_cidr
