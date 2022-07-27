bbc46dab3ccf2322b1327e6de929e51f6a3ad9b7
Frankie Stroud <fstroud@aviatrix.com>
2022-07-27 23:36:13 +1000

    version

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
- 
- module "mc_aws_transit" {
+
+module "mc_aws_transit" {
   #source = "git::https://github.com/terraform-aviatrix-modules/terraform-aviatrix-mc-transit"
-  
-  
-  source  = "terraform-aviatrix-modules/mc-transit/aviatrix/"
-  version = "2.1.6"
+
+  source                 = "terraform-aviatrix-modules/mc-transit/aviatrix"
+  version                = "2.1.6"
   cloud                  = "AWS"
   cidr                   = var.aws_transit_cidr
   region                 = var.aws_region
