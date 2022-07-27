903229df48d9228fcdbc0e501b94b857be5d2cc1
Frankie Stroud <fstroud@aviatrix.com>
2022-07-27 23:06:01 +1000

    region

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+ 
  module "mc_aws_transit" {
   #source = "git::https://github.com/terraform-aviatrix-modules/terraform-aviatrix-mc-transit"
   source  = "terraform-aviatrix-modules/mc-transit/aviatrix//examples/aws_ha"
