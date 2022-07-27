dd127845bb3bd6539ad51a7df1b2f75f2ba75d7a
Frankie Stroud <fstroud@aviatrix.com>
2022-07-27 23:39:56 +1000

    version s Please enter the commit message for your changes. Lines starting

--- variables.tf
+++ variables.tf
@@ -12,28 +12,9 @@ variable "aws_account_name" {
   default = "aws"
 }
 
-variable "admin_email" {
-  default   = ""
-  sensitive = true
-}
 
-variable "controller_kp" {
-  default = "controller_kp"
-}
 
-variable "incoming_ssl_cidr" {
-  type    = list(any)
-  default = ["10.0.0.0/16"]
-}
 
-variable "controller_vpc_cidr_block" {
-  description = "controller vpc cidr block"
-  default     = "10.0.0.0/16"
-}
-
-variable "controller_subnet_cidr" {
-  description = "subnet the controller will be deployed"
-  default     = "10.0.1.0/24"
 
 }
 
